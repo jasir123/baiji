badb460735b3cce1ef69569dee7246e36ee84beb
jasir123 <18201629719@163.com>
2017-03-19 19:59:43 +0800

    百钱买白鸡
    一只公鸡5,一只母鸡3,三只小鸡为1,百钱买白鸡,有多少种组合

---
+++ baiji.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+for x in range(1,20):
+    for y in range(1,32):
+        for z in range(1,93):
+            if x+y+z==100:
+                if 5*x+ 3*y +z/3==100:
+                    print(x,y,z)
+                
